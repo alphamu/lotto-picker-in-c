8cdc7f0ce98e844bb39c4a701ea7e7e09aee1200
atgheb <ali.muzaffar@spinifexgroup.com>
2012-06-29 13:39:35 +1000

    Initial checkin of lotto winning code.

---
+++ lotto.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+
+void die(const char *message) {
+        if(errno) {
+                perror(message);
+        } else {
+                printf("ERROR: %s\n", message);
+        }
+        exit(1);
+}
+
+int duplicate(int *selected, int size,  int number) {
+	int i = 0;
+	for(i = 0; i < size; i++) {
+		//printf("Check number %d, %d = %d\n", i, selected[i], number);
+		if(selected[i] == number) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	unsigned int iseed = (unsigned int)time(NULL);
+	srand(iseed);
+	
+	if(argc < 4) die("Useage: lotto <lowest possible number> <highest possible number> <number of picks> {optional:number of games, default: 1}");
+
+	int min = atoi(argv[1]);
+	if(!min) die("Min number not set");
+
+	int max = atoi(argv[2]);
+	if(!max) die("Max number not set");
+
+	int picks = atoi(argv[3]);
+	if(!picks) die("number of picks not set");
+
+	int selected[picks];
+	memset(selected, 0, picks * sizeof(int));
+//	printf("Count %ld\n", sizeof(selected) / sizeof(int));
+	int repeat = 1;
+	if(argv[4]) {
+		repeat = atoi(argv[4]);
+		if(!repeat) die("repeat could not be set");
+	}
+
+	int i = 0;
+	int j = 0;
+	for(j = 0; j < repeat; j++) {
+		for(i = 0; i < picks; i++) {
+			int selection = rand() % max + min;
+			if(duplicate(selected, picks, selection) == 0) {
+				selected[i] = selection;
+				printf("%d ",selected[i]);
+			} else {
+				i--;
+			}
+			//printf("\n\n\n\n\n");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
+
